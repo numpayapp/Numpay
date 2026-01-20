00a2b7981a25a9214a6da4025c50729e0ad5e44f
abhi8960git <abui8994ty@gmail.com>
2026-01-20 14:40:32 +0530

    chaged rate limiter ( 429 error cause) changed limit to 100 req

--- src/middleware/ratelimiter.ts
+++ src/middleware/ratelimiter.ts
@@ -1,8 +1,8 @@
 import rateLimit from 'express-rate-limit';
 
 export const rateLimiter = rateLimit({
-    windowMs: 5 * 60 * 1000, // 5 minutes
-    max: 10, // limit each IP to 10 requests per windowMs
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 100, // limit each IP to 100 requests per minute
     message: "Too many requests, please try again later."
 });
 
